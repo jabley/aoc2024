9021ba6ddb47a5d4751f81676cfe8c22929b49f1
James Abley <james.abley@gmail.com>
2024-12-01 21:38:00 +0000

    Day 1 optimisations
    * Try to size the vectors only once
    * custom number parsing to omit sign and error handling given my known
      inputs
    
    Really need better profiling setup to see where time is being spent, and
    also to measure what Codspeed will be measuring.

--- src/day01.rs
+++ src/day01.rs
@@ -2,13 +2,14 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
-    let mut left = vec![];
-    let mut right = vec![];
+    let mut left = Vec::with_capacity(1000);
+    let mut right = Vec::with_capacity(1000);
 
     input.lines().for_each(|line| {
-        let mut parts = line
-            .split_whitespace()
-            .map(|val| val.parse::<usize>().unwrap());
+        // Just doing val.parse().unwrap() would be simpler.
+        // But here we play fast and loose with number parsing.
+        // We know our inputs are all positive. So we skip error handling, sign handling and other stuff this way.
+        let mut parts = line.split("   ").map(parse_number);
         left.push(parts.next().unwrap());
         right.push(parts.next().unwrap());
     });
@@ -17,6 +18,18 @@ fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
     right.sort_unstable();
 
     (left, right)
+}
+
+// POWERS_OF_10 is used in tandem with parse_number to try to take advantage of the values in our problem.
+const POWERS_OF_10: [usize; 5] = [1, 10, 100, 1000, 10000];
+
+fn parse_number(val: &str) -> usize {
+    let radix = val.len() - 1;
+
+    val.bytes()
+        .enumerate()
+        .map(|(n, c)| (c - 48) as usize * POWERS_OF_10[radix - n])
+        .sum()
 }
 
 #[aoc(day1, part1)]
@@ -31,29 +44,29 @@ fn part2_impl(sides: &(Vec<usize>, Vec<usize>)) -> usize {
 
     // We have sorted the lists as part of parsing. Chunk by the distinct values in each list and
     // make it peekable because there aren't any side-effects here to be concerned about.
-    let mut a = left.chunk_by(|x, y| x == y).peekable();
-    let mut b = right.chunk_by(|x, y| x == y).peekable();
+    let mut left = left.chunk_by(|x, y| x == y).peekable();
+    let mut right = right.chunk_by(|x, y| x == y).peekable();
 
     let mut sum = 0;
 
     loop {
-        let Some(&x) = a.peek() else {
+        let Some(&x) = left.peek() else {
             break;
         };
-        let Some(&y) = b.peek() else {
+        let Some(&y) = right.peek() else {
             break;
         };
         match x[0].cmp(&y[0]) {
             std::cmp::Ordering::Less => {
-                a.next();
+                left.next();
             }
             std::cmp::Ordering::Equal => {
                 sum += x[0] * x.len() * y.len();
-                a.next();
-                b.next();
+                left.next();
+                right.next();
             }
             std::cmp::Ordering::Greater => {
-                b.next();
+                right.next();
             }
         }
     }
@@ -102,5 +115,10 @@ mod tests {
             )),
             31
         );
+    }
+
+    #[test]
+    fn hand_parsing() {
+        assert_eq!(91527, parse_number("91527"))
     }
 }
