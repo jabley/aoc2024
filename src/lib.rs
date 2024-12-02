ab0da8b4c8521284f722c0a10b1211c98959f811
James Abley <james.abley@gmail.com>
2024-12-02 19:45:01 +0000

    Avoid `bytes()`
    I think this avoids a memory copy [1]? Minor improvement in day02
    performance as a result.
    
    [1] https://doc.rust-lang.org/src/core/str/mod.rs.html#919

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@ const POWERS_OF_10: [usize; 5] = [1, 10, 100, 1000, 10000];
 fn parse_number(val: &str) -> usize {
     let radix = val.len() - 1;
 
-    val.bytes()
+    val.as_bytes()
+        .iter()
         .enumerate()
         .map(|(n, c)| (c - 48) as usize * POWERS_OF_10[radix - n])
         .sum()
