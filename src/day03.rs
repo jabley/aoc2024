7a54f9617686d0646fe85e8b17b9c47bb8d14546
James Abley <james.abley@gmail.com>
2024-12-04 09:08:54 +0000

    Make day 3 faster
    Regular expressions are great and the Rust regex crate rocks. But a more
    targetted parser will usually outperform it.
    
    Here I'm using winnow, but could have used nom as well.
    
    This gives ~60% performance improvement on criterion benchmarking.

--- src/day03.rs
+++ src/day03.rs
@@ -1,74 +1,106 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use regex::bytes::Regex;
+use winnow::{
+    ascii::dec_uint,
+    combinator::{delimited, separated_pair},
+    PResult, Parser,
+};
 
-#[aoc_generator(day3)]
-fn parse(input: &str) -> String {
-    input.to_string()
-}
+#[derive(Eq, PartialEq, Debug)]
+struct Mul((u32, u32));
 
-#[aoc(day3, part1)]
-fn part1_impl(input: &str) -> u32 {
-    let rx = Regex::new(r"mul\(\d+,\d+\)").unwrap();
-    rx.find_iter(input.as_bytes())
-        .map(|matches| eval_mul_expression(matches.as_bytes()))
-        .sum()
-}
-
-/// Given an expression `mul(number1,number2)`, evaluate it.
-fn eval_mul_expression(expression: &[u8]) -> u32 {
-    // skip the b"mul(" and trailing b")", then split into 2 numeric strings
-    expression[4..expression.len() - 1]
-        .split(|b| *b == b',')
-        .map(parse_u16)
-        .reduce(|acc, val| val * acc)
-        .unwrap() as u32
-    // let first = parse_u16(parts.next().unwrap());
-    // let second = parse_u16(parts.next().unwrap());
-    // (first * second) as u32
-}
-
-fn parse_u16(v: &[u8]) -> u16 {
-    // In our problem input, we only have 1, 2, or 3 digit numbers. Optimise for that.
-    match v.len() {
-        // b'8' => 8_u16
-        1 => unsafe { (v.get_unchecked(0) - 48) as u16 },
-        // b'85' => (8 * 10 + 5)_u16
-        2 => unsafe { (v.get_unchecked(0) - 48) as u16 * 10 + (v.get_unchecked(1) - 48) as u16 },
-        // b'785' => (7 * 100 + 8 * 10 + 5)_u16
-        3 => unsafe {
-            (v.get_unchecked(0) - b'0') as u16 * 100
-                + (v.get_unchecked(1) - b'0') as u16 * 10
-                + (v.get_unchecked(2) - b'0') as u16
-        },
-        _ => panic!("Not parsing '{:?}' into a u16", v),
+impl Mul {
+    fn eval(&self) -> u32 {
+        self.0 .0 * self.0 .1
     }
 }
 
-#[aoc(day3, part2)]
-fn part2_impl(input: &str) -> u32 {
-    let rx = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
-    rx.find_iter(input.as_bytes())
-        .map(|rx_match| rx_match.as_bytes())
-        .fold((0, true), |(acc, enabled), instruction| match instruction {
-            b"do()" => (acc, true),
-            b"don't()" => (acc, false),
-            _ => {
-                if enabled {
-                    (acc + eval_mul_expression(instruction), true)
-                } else {
-                    (acc, enabled)
-                }
+/// Parses something like 123,456
+fn number_pair(i: &mut &str) -> PResult<(u32, u32)> {
+    separated_pair(dec_uint, ',', dec_uint).parse_next(i)
+}
+
+/// Parses a mul instruction like `mul(123, 44)`
+fn mul(i: &mut &str) -> PResult<Mul> {
+    delimited("mul(", number_pair, ')').map(Mul).parse_next(i)
+}
+
+#[aoc_generator(day3, part1)]
+fn parse_part1(input: &str) -> Vec<Mul> {
+    let mut res = Vec::with_capacity(8);
+
+    let mut i = 0;
+
+    while i < input.len() {
+        let mut suffix = &input[i..];
+        let n = suffix.len();
+
+        if let Ok(mul) = mul(&mut suffix) {
+            res.push(mul);
+            let chars_parsed = n - suffix.len();
+            i += chars_parsed;
+            continue;
+        }
+
+        i += 1;
+    }
+
+    res
+}
+
+#[aoc(day3, part1)]
+fn part1_impl(input: &[Mul]) -> u32 {
+    input.iter().map(|m| m.eval()).sum()
+}
+
+#[aoc_generator(day3, part2)]
+fn parse_part2(input: &str) -> Vec<Mul> {
+    let mut res = Vec::with_capacity(8);
+
+    let mut i = 0;
+
+    let mut enabled = true;
+
+    while i < input.len() {
+        if input[i..].starts_with("do()") {
+            enabled = true;
+            i += 4;
+            continue;
+        }
+        if input[i..].starts_with("don't()") {
+            enabled = false;
+            i += 7;
+            continue;
+        }
+
+        let mut suffix = &input[i..];
+        let n = suffix.len();
+
+        if enabled {
+            if let Ok(mul) = mul(&mut suffix) {
+                res.push(mul);
+                let chars_parsed = n - suffix.len();
+                i += chars_parsed;
+                continue;
             }
-        })
-        .0
+        }
+
+        i += 1;
+    }
+
+    res
+}
+
+#[aoc(day3, part2)]
+fn part2_impl(input: &[Mul]) -> u32 {
+    input.iter().map(|m| m.eval()).sum()
 }
 
 pub fn part1(input: &str) -> u32 {
-    part1_impl(&parse(input))
+    part1_impl(&parse_part1(input))
 }
 
 pub fn part2(input: &str) -> u32 {
-    part2_impl(&parse(input))
+    part2_impl(&parse_part2(input))
 }
 
 #[cfg(test)]
@@ -78,9 +110,7 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(
-            part1_impl(&parse(
-                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-            )),
+            part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
             161
         );
     }
@@ -88,9 +118,7 @@ mod tests {
     #[test]
     fn part2_example() {
         assert_eq!(
-            part2_impl(&parse(
-                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-            )),
+            part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"),
             48
         );
     }
